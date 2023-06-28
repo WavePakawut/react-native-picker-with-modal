c7fe573fc0ca2de36b40cc81d124eb5bd08399ac
Pakawut Maleephet <wave51040@hotmail.com>
2023-06-28 10:34:24 +0700

    feat: change pakage user name

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply, Picker } from '@dph/react-native-picker-with-modal';
+import { multiply, Picker } from '@wavedph/react-native-picker-with-modal';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
